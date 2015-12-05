10bb9ce19e15703c0fb5876e88baaea553d758d8
Jim Cummins <jcummins@fisdap.net>
2015-12-05 09:47:47 -0600

    use an example config file so people don't accidentally check in keys

--- src/lib/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  PARSE: {
-    APP_ID: '9H8pOkHqCgRfrboq63PwMgjsQaJwjFw',
-    REST_API_KEY: 'yPWk7VdQIoltlbeCWRr8Hh8b29hlJ3A',
-    SESSION_TOKEN_KEY: 'SESSION_TOKEN_KEY'
-  }
-}
