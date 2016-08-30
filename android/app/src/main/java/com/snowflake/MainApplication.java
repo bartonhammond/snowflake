5279b429c637a97067233b599beb5875d4d6b02c
Barton Hammond <bartonhammond@gmail.com>
2016-08-30 10:55:39 -0500

    upgrade to newest releases

--- android/app/src/main/java/com/snowflake/MainApplication.java
+++ android/app/src/main/java/com/snowflake/MainApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.burnweb.rnsimplealertdialog.RNSimpleAlertDialogPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -11,13 +14,6 @@ import com.facebook.react.shell.MainReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
-
-// Add this line:
-import com.oblador.vectoricons.VectorIconsPackage;
-// Add this line:
-import com.burnweb.rnsimplealertdialog.RNSimpleAlertDialogPackage;  
-// Add this line:
-import com.i18n.reactnativei18n.ReactNativeI18n;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -30,12 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-                                         new MainReactPackage(),
-                                         //Added two lines
-                                         new RNSimpleAlertDialogPackage(),
-                                         new VectorIconsPackage(),
-                                         new ReactNativeI18n()
-                                         );
+          new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNSimpleAlertDialogPackage(),
+            new ReactNativeI18n()
+      );
     }
   };
 
