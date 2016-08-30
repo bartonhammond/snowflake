3f2da283e719e33f0e7409d80e6d423ee5102b4f
Barton Hammond <bartonhammond@gmail.com>
2016-08-30 13:28:45 -0500

    Fixes #133

--- android/app/src/main/java/com/snowflake/MainApplication.java
+++ android/app/src/main/java/com/snowflake/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.burnweb.rnsimplealertdialog.RNSimpleAlertDialogPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.facebook.react.ReactInstanceManager;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
             new RNSimpleAlertDialogPackage(),
             new ReactNativeI18n()
       );
